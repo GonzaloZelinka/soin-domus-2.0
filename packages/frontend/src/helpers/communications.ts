415f173212b7900c49305c914aedff2bda477687
Juan S <segnanajuan@gmail.com>
2022-11-20 11:40:21 -0300

    fix communication

--- packages/frontend/src/helpers/communications.ts
+++ packages/frontend/src/helpers/communications.ts
@@ -1,5 +1,5 @@
 import { API_URL } from './config'
-import { I_Inquilino } from 'shared-common'
+import { IPropiedad, I_Inquilino } from 'shared-common'
 import axios from 'axios'
 
 const crearNuevoInquilino = async (inquilino: I_Inquilino) => {
@@ -7,9 +7,18 @@ const crearNuevoInquilino = async (inquilino: I_Inquilino) => {
   return inq.data
 }
 
+interface IPropiedadResponse {
+  output: {
+    propiedades: IPropiedad[]
+  }
+  message: string
+}
+
 const getInfoProperty = async (value: string, type: 'inquilino' | 'property') => {
-  const props = await axios.get(`${API_URL}/propiedades`, { params: { type, value } })
-  return props.data
+  const props = await axios.get<IPropiedadResponse>(`${API_URL}/propiedades`, { params: { type, value } })
+  const { output, message } = props.data;
+  console.log(message)
+  return output?.propiedades ?? []
 }
 
 export { crearNuevoInquilino, getInfoProperty }
